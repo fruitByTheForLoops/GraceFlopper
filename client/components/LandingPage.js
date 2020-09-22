139caf21f8aa1253eb3e9443bceecebcc936930c
Carlo Rossy <carlorossy88@gmail.com>
2020-09-22 16:19:37 -0400

    feature: created landing component,routes, and implemented bootstrap.js and css with carousel

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -2,6 +2,59 @@ import React, {Component} from 'react'
 
 export default class LandingPage extends Component {
   render() {
-    return <div />
+    return (
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide carousel-images"
+        data-ride="carousel"
+      >
+        <ol className="carousel-indicators">
+          <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to="0"
+            className="active"
+          />
+          <li data-target="#carouselExampleIndicators" data-slide-to="1" />
+          <li data-target="#carouselExampleIndicators" data-slide-to="2" />
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              className="d-block w-100"
+              src="https://bs.floristic.org/image/m/8202f68a42d7bbaed4c668f2ba76139b46bd05fd"
+              alt="First slide"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="d-block w-100"
+              src="https://www.greenbiz.com/sites/default/files/images/articles/featured/seedsnopparatpromthasstock.png"
+              alt="Second slide"
+            />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="..." alt="Third slide" />
+          </div>
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+    )
   }
 }
