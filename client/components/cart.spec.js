326ab97c3f75479fd6478ddef46425cb2c9088ab
Kevin Hu <hukxchen+git@gmail.com>
2020-09-22 11:19:05 -0500

    :heavy_minus_sign: stay with mocha, remove tape
    because boilermaker has already written tests with mocha - so it's all good

--- client/components/cart.spec.js
+++ client/components/cart.spec.js
@@ -1,1 +1,14 @@
-import test from 'tape'
+/* global describe beforeEach it */
+
+import {expect} from 'chai' // assertion library
+import React from 'react'
+import enzyme, {shallow} from 'enzyme' //
+import Adapter from 'enzyme-adapter-react-16'
+import {UserHome} from './user-home'
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe('Cart', () => {
+  let cart
+})
