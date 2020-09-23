86bc10091e4aa66d2ab6c409742ad86805036b34
Kevin Hu <hukxchen+git@gmail.com>
2020-09-23 15:08:13 -0500

    update database model so that price is in pennies

--- server/db/models/fruitySeed.js
+++ server/db/models/fruitySeed.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
+//pricePerUnit in pennies
 const FruitySeed = db.define('fruityseed', {
   name: {
     type: Sequelize.STRING,
@@ -8,7 +9,7 @@ const FruitySeed = db.define('fruityseed', {
     validate: {notEmpty: true},
   },
   pricePerUnit: {
-    type: Sequelize.FLOAT,
+    type: Sequelize.INTEGER,
     allowNull: false,
   },
   seedsPerUnit: {
