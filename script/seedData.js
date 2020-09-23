240e66adfd59b8d0e94963c5af4f95a23b9bbe27
Kevin Hu <hukxchen+git@gmail.com>
2020-09-23 15:22:19 -0500

    Export fetchData as a function to our seed file

--- script/seedData.js
+++ script/seedData.js
@@ -1,8 +1,9 @@
 const fetch = require('node-fetch')
+const TREFLE_TOKEN = process.env.TREFLE_TOKEN
 
 const fetchData = async () => {
   const response = await fetch(
-    'https://trefle.io/api/v1/species/search?token=t5LZvZ7RSAwz6MMk9joiLdB5osOQ1zyTC2VvvXOvoHk&q=apple'
+    `https://trefle.io/api/v1/species/search?token=${TREFLE_TOKEN}&q=apple`
   )
   const {data} = await response.json()
   const filteredData = data.map((el) => ({
@@ -10,15 +11,15 @@ const fetchData = async () => {
     image_url: el.image_url,
     links: el.links.self,
   }))
-  for (let i = 0; i < filteredData.length; i++) {
-    const detailedData = await fetch(
-      `https://trefle.io${filteredData[i].links}?token=t5LZvZ7RSAwz6MMk9joiLdB5osOQ1zyTC2VvvXOvoHk`
-    )
-    const {data: detail} = await detailedData.json()
-    console.log(detail.growth.light)
-  }
+  /* In progress. TODO: fetch detailed data per plant */
+  // for (let i = 0; i < filteredData.length; i++) {
+  //   const detailedData = await fetch(
+  //     `https://trefle.io${filteredData[i].links}?token=${TREFLE_TOKEN}`
+  //   )
+  //   const {data: detail} = await detailedData.json()
+  //   console.log(detail.growth.light)
+  // }
+  return filteredData
 }
 
-//     `https://trefle.io${el.links}}?token=t5LZvZ7RSAwz6MMk9joiLdB5osOQ1zyTC2VvvXOvoHk`
-
-fetchData()
+module.exports = fetchData
