2b2d441e37685047019c0aa87bdb6bc8e4d10a75
Carlo Rossy <carlorossy88@gmail.com>
2020-09-24 18:07:40 -0400

    feat: added cart comp. and cart.spec'

--- client/components/Carts.js
+++ client/components/Carts.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import {connect} from 'react-redux'
+import {fetchCarts} from '../store/index.js'
 
-const Carts = ({carts}) => {
-  return (
-    <>
-      <h1>Your Cart</h1>
-    </>
-  )
+import React, {Component} from 'react'
+
+export class Carts extends Component {
+  // component did mount, fetch of carts
+  componentDidMount() {
+    this.props.fetchInitialCarts(this.props.id)
+  }
+
+  render() {
+    // in render, map our fetched cart, and provide a conditional
+
+    return (
+      <div>
+        {this.props.carts.map((cart) => {
+          if (!cart.checkedOut) {
+            //render with css styling from bootstrap (card)
+            cart.productsInCart.map((product) => (
+              <singleProductView product={product} />
+            ))
+            //delete button
+          }
+        })}
+      </div>
+    )
+  }
+}
+//map our state
+//map our action creators
+// connect our redux
+const mapState = (state) => {
+  return {
+    carts: state.carts,
+    id: state.user.id,
+  }
 }
 
-export default Carts
+const mapDispatch = (dispatch) => {
+  return {
+    fetchInitialCarts: () => dispatch(fetchCarts()),
+  }
+}
+
+export default connect(mapState, mapDispatch)(Carts)
