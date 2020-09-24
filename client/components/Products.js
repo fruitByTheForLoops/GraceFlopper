ac9712c554605c6ee36e174e3b45023e6655fc58
jcho42 <jason.cho.301230@gmail.com>
2020-09-24 14:15:26 -0500

    feat: add connect to Products component

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchProducts} from '../store/products'
 
-export default class Products extends Component {
+class Products extends Component {
+  componentDidMount() {
+    this.props.fetch()
+  }
   render() {
+    const products = this.props.products || []
+    console.log('products ---->', products)
     return (
       <div>
         <nav aria-label="breadcrumb">
@@ -14,23 +21,36 @@ export default class Products extends Component {
             </li>
           </ol>
         </nav>
-        {/* <div className="all-products">
-          <div class="card" style="width: 18rem;">
+        {!products.id && <div>Loading...</div>}
+        <div className="all-products">
+          {products.map((product) => (
+            <div className="card" style="width: 18rem;">
               we need to map our products to list in cards
-              <img src="..." class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="btn btn-primary">
-                Go somewhere
-              </a>
+              <img src={product.imageUrl} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
             </div>
-          </div>
-        </div> */}
+          ))}
+        </div>
       </div>
     )
   }
 }
+
+const mapState = (state) => ({
+  products: state.products,
+})
+
+const mapDispatch = (dispatch) => ({
+  fetch: () => dispatch(fetchProducts()),
+})
+
+export default connect(mapState, mapDispatch)(Products)
