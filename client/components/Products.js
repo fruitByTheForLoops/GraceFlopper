ca0b837fb77de79bb4e4897e8032366540141bf5
Jeff_Lee_0228 <suam3282@gmail.com>
2020-09-24 22:32:14 -0500

    fix: fixed loading message to be placed below breadcrumb and fixed style.css files.

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchProducts} from '../store/products'
+import {Link} from 'react-router-dom'
 
 class Products extends Component {
   componentDidMount() {
     this.props.fetch()
   }
   render() {
-    const products = this.props.products || []
-    {
-      if (!Array.isArray(products)) return <div>Loading...</div>
-    }
-    console.log('products ---->', products)
+    const products = this.props.products
+    // {
+    //   if (!Array.isArray(products)) return <div>Loading...</div>
+    // }
     return (
       <div>
         <nav aria-label="breadcrumb">
@@ -24,29 +24,33 @@ class Products extends Component {
             </li>
           </ol>
         </nav>
-        <div className="all-products">
-          {products.map((product) => (
-            <div key={product.id} className="card">
-              <img
-                src={product.imageUrl}
-                className="card-img-top"
-                alt={product.name}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{product.name}</h5>
-                <ul>
-                  <li>Price: ${product.pricePerUnit / 100}</li>
-                  <li>
-                    {product.seedsPerUnit} seeds per {product.unit}
-                  </li>
-                </ul>
-                <button className="add-to-cart-button" type="button">
-                  Add To Cart
-                </button>
+        {Array.isArray(products) ? (
+          <div className="all-products">
+            {products.map((product) => (
+              <div key={product.id} className="card">
+                <Link to={`/shop/${product.id}`}>
+                  <img
+                    src={product.imageUrl}
+                    className="card-img-top"
+                    alt={product.name}
+                  />
+                </Link>
+                <div className="card-body">
+                  <h5 className="card-title">{product.name}</h5>
+                  <h6>Price: ${product.pricePerUnit / 100}</h6>
+                  <h6>
+                    {product.seedsPerUnit} seeds / {product.unit}
+                  </h6>
+                  <button className="add-to-cart-button" type="button">
+                    Add To Cart
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     )
   }
