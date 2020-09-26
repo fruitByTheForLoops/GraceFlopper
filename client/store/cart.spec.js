61bf3c2c97f8804c7f5d8b54986b6b9cea39401b
Kevin Hu <hukxchen+git@gmail.com>
2020-09-25 20:27:18 -0500

    test: get carts thunk creator #17
    This in service of being able to get carts into the redux store for a
    logged in user. It is in service of the Cart View and the Logged in
    customer being able to have a persistent cart between sessions.

--- client/store/cart.spec.js
+++ client/store/cart.spec.js
@@ -1,49 +1,43 @@
 /* global describe beforeEach afterEach it */
 
-// import {expect} from 'chai'
-// import axios from 'axios'
-// import MockAdapter from 'axios-mock-adapter'
-// import configureMockStore from 'redux-mock-store'
-// import thunkMiddleware from 'redux-thunk'
-// import history from '../history'
-//
-// const middlewares = [thunkMiddleware]
-// const mockStore = configureMockStore(middlewares)
-//
-// describe('thunk creators', () => {
-//   let store
-//   let mockAxios
-//
-//   const initialState = {cart: {}}
-//
-//   beforeEach(() => {
-//     mockAxios = new MockAdapter(axios)
-//     store = mockStore(initialState)
-//   })
-//
-//   afterEach(() => {
-//     mockAxios.restore()
-//     store.clearActions()
-//   })
-//
-//   describe('add', () => {
-//     it('eventually dispatches the GET USER action', async () => {
-//       const fakeUser = {email: 'Cody'}
-//       mockAxios.onGet('/auth/me').replyOnce(200, fakeUser)
-//       await store.dispatch(me())
-//       const actions = store.getActions()
-//       expect(actions[0].type).to.be.equal('GET_USER')
-//       expect(actions[0].cart).to.be.deep.equal(fakeUser)
-//     })
-//   })
-//
-//   describe('logout', () => {
-//     it('logout: eventually dispatches the REMOVE_USER action', async () => {
-//       mockAxios.onPost('/auth/logout').replyOnce(204)
-//       await store.dispatch(logout())
-//       const actions = store.getActions()
-//       expect(actions[0].type).to.be.equal('REMOVE_USER')
-//       expect(history.location.pathname).to.be.equal('/login')
-//     })
-//   })
-// })
+import {expect} from 'chai'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import configureMockStore from 'redux-mock-store'
+import thunkMiddleware from 'redux-thunk'
+import history from '../history'
+
+const middlewares = [thunkMiddleware]
+const mockStore = configureMockStore(middlewares)
+
+describe('action creators', () => {
+  let store
+})
+
+describe('thunk creators', () => {
+  let store
+  let mockAxios
+
+  const initialState = {cart: {}}
+
+  beforeEach(() => {
+    mockAxios = new MockAdapter(axios)
+    store = mockStore(initialState)
+  })
+
+  afterEach(() => {
+    mockAxios.restore()
+    store.clearActions()
+  })
+
+  describe('carts and previous orders', () => {
+    it('should dispatch a GET_CARTS action', async () => {
+      const fakeCarts = []
+      mockAxios.onGet('/carts/:id').replyOnce(200, fakeCarts)
+      await store.dispatch(carts())
+      const actions = store.getActions()
+      expect(actions[0].type).to.be.equal('GET_CARTS')
+      expect(actions[0].carts).to.be.deep.equal(fakeCarts)
+    })
+  })
+})
