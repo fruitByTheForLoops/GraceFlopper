830934ca5434694b26b38a3fbe0818571c06b85e
Kevin Hu <hukxchen+git@gmail.com>
2020-09-26 22:32:55 -0400

    test: that the fetchCarts thunk creator dispatches the correct action
    the correct action has a type GET_CARTS and a carts property which is an array

--- client/store/cart.spec.js
+++ client/store/cart.spec.js
@@ -1,24 +1,21 @@
 /* global describe beforeEach afterEach it */
 
 import {expect} from 'chai'
+import {fetchCarts} from './cart'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
 import thunkMiddleware from 'redux-thunk'
-import history from '../history'
 
 const middlewares = [thunkMiddleware]
 const mockStore = configureMockStore(middlewares)
 
-describe('action creators', () => {
-  let store
-})
-
+/* TEST THUNK CREATORS */
 describe('thunk creators', () => {
   let store
   let mockAxios
 
-  const initialState = {cart: {}}
+  const initialState = {}
 
   beforeEach(() => {
     mockAxios = new MockAdapter(axios)
@@ -30,12 +27,15 @@ describe('thunk creators', () => {
     store.clearActions()
   })
 
-  describe('carts and previous orders', () => {
+  // Grabs all the carts from the database
+  describe('fetchCarts', () => {
     it('should dispatch a GET_CARTS action', async () => {
       const fakeCarts = []
-      mockAxios.onGet('/carts/:id').replyOnce(200, fakeCarts)
-      await store.dispatch(carts())
+      mockAxios.onGet('/api/users/1/carts').replyOnce(200, fakeCarts)
+      await store.dispatch(fetchCarts(1))
+      console.log('mock store state', store.getState())
       const actions = store.getActions()
+      console.log('actions --> ', actions)
       expect(actions[0].type).to.be.equal('GET_CARTS')
       expect(actions[0].carts).to.be.deep.equal(fakeCarts)
     })
