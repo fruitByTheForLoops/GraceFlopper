064a43cf398d8d9cac9b3abdb0dc8cfeab73c847
Carlo Rossy <carlorossy88@gmail.com>
2020-09-28 16:46:24 -0400

    feat:checked Out functionality added

--- client/components/Carts.js
+++ client/components/Carts.js
@@ -1,20 +1,37 @@
 import {connect} from 'react-redux'
-import {fetchCarts} from '../store/index.js'
+import {fetchCarts, updatedCartToServer} from '../store/index.js'
 
 import React, {Component} from 'react'
 
 export class Carts extends Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
   // component did mount, fetch of carts
-
+  handleClick(ev) {
+    ev.preventDefault()
+    const cartId = ev.target.value
+    this.props.updateCheckout(cartId)
+  }
   render() {
     // in render, map our fetched cart, and provide a conditional
     console.log('this.props.carts --->', this.props.carts)
     const fruityseeds = Object.values(this.props.carts.activeCart.fruityseeds)
     return (
       <div>
-        {fruityseeds.map((fruityseed) => (
-          <div>{fruityseed.name}</div>
-        ))}
+        <div>
+          {fruityseeds.map((fruityseed) => (
+            <div>{fruityseed.name}</div>
+          ))}
+        </div>
+        <button
+          type="button"
+          value={this.props.carts.activeCart.id}
+          onClick={this.handleClick}
+        >
+          Check Out
+        </button>
       </div>
     )
   }
@@ -32,6 +49,7 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     fetchInitialCarts: (id) => dispatch(fetchCarts(id)),
+    updateCheckout: (cartId) => dispatch(updatedCartToServer(cartId)),
   }
 }
 
--- server/api/carts.js
+++ server/api/carts.js
@@ -10,3 +10,21 @@ router.get('/', async (req, res, next) => {
     next(error)
   }
 })
+router.put('/:id', async (req, res, next) => {
+  try {
+    console.log(req.params.id)
+    await Cart.update(
+      {checkedOut: true},
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+    let updatedCart = await Cart.findByPk(req.params.id)
+    console.log(updatedCart)
+    res.send(updatedCart)
+  } catch (error) {
+    next(error)
+  }
+})
