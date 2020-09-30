eb704e37d105a0ad776f01a3fe2d99c2dafafbaa
Kevin Hu <hukxchen+git@gmail.com>
2020-09-29 22:24:33 -0400

    fix: remove console.log statements
    in the interest of actually giving the appropriate 401 error

--- server/isAdmin.js
+++ server/isAdmin.js
@@ -1,6 +1,4 @@
 module.exports = (req, res, next) => {
-  console.log('The User --> ', req.user)
-  console.log('isAdmin --> ', req.user.isAdmin)
   const currentUser = req.user
   if (currentUser && currentUser.isAdmin) {
     next()
